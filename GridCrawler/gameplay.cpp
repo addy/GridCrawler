e815a921a89758e2af88ce1af6b7ee746775d472
Richard <richard.habeeb@gmail.com>
2013-06-26 12:59:31 -0500

    Moved all timing and video logic to gameplay from dungeon.

--- GridCrawler/gameplay.cpp
+++ GridCrawler/gameplay.cpp
@@ -2,12 +2,60 @@
 #include "gameplay.h"
 
 GamePlay::GamePlay(void) {
+	window = new sf::RenderWindow(sf::VideoMode(WINDOW_HEIGHT_CELLS*CELL_HEIGHT_PX, WINDOW_WIDTH_CELLS*CELL_WIDTH_PX), "Grid Crawler");
+	checkKeyNextTime = clk.restart();
+	updateWindowNextTime = clk.getElapsedTime();
+
 	player = new Entity(PLAYER_TYPE);
 	
 	player->setTexture("Brain Jelly.png");
 
 	currentMap = new Map(10, 10, player, DUNGEON_MAP);
 }
+
+
+void GamePlay::play(void) {
+
+	while (window->isOpen())
+	{
+		//Check to see if movement animation is completed??
+		if(checkKeyNextTime <= clk.getElapsedTime() && checkKeys())
+			checkKeyNextTime = clk.getElapsedTime()+sf::milliseconds(100);
+
+		handleAnimation();
+		handleMapEnvironment();
+
+
+		//update the screen at a maximum of a certain times per second
+		if(updateWindowNextTime <= clk.getElapsedTime()) {
+			window->clear(sf::Color::Black);
+
+			draw(window);
+			
+			window->display();
+			prevUpdateWindowTime = updateWindowNextTime;
+			updateWindowNextTime = clk.getElapsedTime()+sf::milliseconds((int)(1000/MAX_FPS)); //current time + milliseconds per frame
+		}
+
+
+		sf::Event event;
+		while (window->pollEvent(event))
+		{
+			// Close window : exit
+			if (event.type == sf::Event::Closed)
+				window->close();
+		}
+
+	}
+}
+
+
+
+int GamePlay::endGame(void) {
+	//free all memory.
+	return 0;
+}
+
 
 
 bool GamePlay::checkKeys(void) {
@@ -38,9 +86,7 @@ bool GamePlay::checkKeys(void) {
 
 void GamePlay::draw(sf::RenderWindow* window) {
 	//ANIMATION AND VIDEO STUFFS. HANDLE ANY ANIMATION / SPRITE MOVEMENTS
-
-	
-	(*window).draw(*player->getSprite());
+	window->draw(*player->getSprite());
 
 
 }
--- GridCrawler/gameplay.h
+++ GridCrawler/gameplay.h
@@ -5,13 +5,23 @@
 #include <SFML\Window.hpp>
 #include "map.h"
 
+//MAX FPS
+const double MAX_FPS = 100;
+
 class GamePlay {
 private:
 	Map* currentMap;
 	Entity* player;
+	sf::RenderWindow* window;
+	sf::Clock clk;
+	sf::Time checkKeyNextTime;
+	sf::Time updateWindowNextTime;
+	sf::Time prevUpdateWindowTime;
 
 public:
 	GamePlay(void);
+	void play(void);
+	int endGame(void);
 	bool checkKeys(void);
 	void draw(sf::RenderWindow*);
 	void handleAnimation(void);
